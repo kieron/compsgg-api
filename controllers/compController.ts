907ace865544dd8949c68a193b09b43e0a47170e
kieron <kieronboz@gmail.com>
2024-05-13 09:15:24 +0100

    feat: Add pagination to getComps function

--- controllers/compController.ts
+++ controllers/compController.ts
@@ -7,7 +7,12 @@ import { Comp } from "../models";
  * @access Public
  */
 export const getComps = async (c: Context) => {
-  const comps = await Comp.find().sort("-_id").limit(50);
+  const limit = parseInt(c.req.query("limit") || "50");
+  const page = parseInt(c.req.query("page") || "1");
+  const comps = await Comp.find()
+    .sort("-_id")
+    .skip((page - 1) * limit)
+    .limit(limit);
 
   return c.json({ comps });
 };
